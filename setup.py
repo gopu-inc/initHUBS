81bbda1d9e212d981ae55f328ce6eaf8f6d352f5
Mauricio-100 <ceoseshell@gmail.com>
2025-11-27 10:08:45 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     install_requires=requirements,
     entry_points={
         "console_scripts": [
-            "inithub=inithub.cli:main",
+            "inithub=cli:main",
         ],
     },
     include_package_data=True,
